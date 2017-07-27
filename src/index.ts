c366fb75ef42134261765bf62e7df659a8a2113a
Peter Szabo <peter.szabo13@gmail.com>
2017-07-27 16:36:37 +0200

    Switch to default HTTP server

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,13 @@
 import app from './App'
+import * as http from "http";
 
 const port = process.env.PORT || 5000;
+const server: http.Server = http.createServer(app);
 
-app.listen(port, (err: any) => {
-    if (err) {
-        return console.log(err)
-    }
-
-    return console.log(`server is listening on ${port}`)
+server.listen(port);
+server.on("error", (error: Error) => {
+    console.error(`Error starting  server ${error}`);
+});
+server.on("listening", () => {
+    console.log(`Server started on port ${port}`);
 });
