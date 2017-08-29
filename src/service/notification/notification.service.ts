7da509b1ea3bd220d6d25c1e15da5bb5e5b8de98
Franclin Cabral <franclincabral@gmail.com>
2017-08-29 21:33:28 +0200

    Tests and business logic for notification.

--- src/service/notification/notification.service.ts
+++ src/service/notification/notification.service.ts
@@ -1,29 +1,36 @@
 import config from "../../../config/config";
 import * as admin from "firebase-admin";
+import NotificationInterface from "./notification.interface";
+
+const FEEDBACK_TEMPLATE = "FEEDBACK";
+const REQUEST_TEMPLATE = "REQUEST";
+const COMPASS_TEMPLATE = "COMPASS";
 
 export default class NotificationService {
 
+    service : any;
 
-
-    constructor(){
-        var serviceAccount = require(`res/${config.firebaseConfig.keyFileName}`);
-        admin.initializeApp({
-            credential: admin.credential.cert(serviceAccount),
-            databaseURL: "https://plenuumbackend.firebaseio.com"
-        });
+    constructor(service: NotificationInterface){
+        this.service = service;
     }
 
-    getPayLoad(){
-        return {
-            notification: {
-                title: "",
-                body: ""
-            }
-        };
+    static getNotificationFromTemplate(senderName: string, template: string){
+        switch (template){
+            case FEEDBACK_TEMPLATE:
+                return { body: `You received a feedback from ${senderName}`, title: `New feedback`};
+            case REQUEST_TEMPLATE:
+                return { body: `${senderName} needs your feedback`, title: `New feedback request`};
+            case COMPASS_TEMPLATE:
+                return { body: `Would you like to help ${senderName} to improve?`, title: `New todo`};
+            default:
+                return { body: "", title: "" };
+        }
     }
 
-    sendNotification(){
-        const condition = ""
-        const payload = this.getPayLoad();
+    sendNotification(deviceToken: string, senderName: string, notificationType: string): Promise<any> {
+        const content = NotificationService.getNotificationFromTemplate(senderName, notificationType);
+        return this.service.sendNotification(deviceToken, content)
+            .then((response: any) => response)
+            .catch((err: Error) => err);
     }
 }
--- src/test/service/notification.service.spec.ts
+++ src/test/service/notification.service.spec.ts
@@ -4,9 +4,87 @@ import { expect } from "chai";
 import NotificationService from "../../service/notification/notification.service";
 
 suite("Notification tests", () => {
+    const mockToken = "217c98da-92b0-48ca-b97e-22ee55a6eeb7";
 
-    suite("", () => {
+    suite("Happy cases", () => {
 
+        let notificationService : NotificationService;
+        before(() => {
+            const mockService :any = {
+                sendNotification: sinon.stub().resolves("mock response")
+            };
+            notificationService = new NotificationService(mockService);
+        });
+
+        test("Get Notification From template Feedback", () => {
+            const expectedResult = {
+                body: `You received a feedback from ${testUser.firstName}`,
+                title: `New feedback`
+            };
+            const result = NotificationService.getNotificationFromTemplate(testUser.firstName, "FEEDBACK");
+            expect(result).to.deep.equal(expectedResult);
+        });
+
+        test("Get Notification From template Request", () => {
+            const expectedResult = {
+                body: `${testUser.firstName} needs your feedback`,
+                title: `New feedback request`
+            };
+            const result = NotificationService.getNotificationFromTemplate(testUser.firstName, "REQUEST");
+            expect(result).to.deep.equal(expectedResult);
+        });
+
+        test("Get Notification From template Compass", () => {
+            const expectedResult = {
+                body: `Would you like to help ${testUser.firstName} to improve?`,
+                title: `New todo`
+            };
+            const result = NotificationService.getNotificationFromTemplate(testUser.firstName, "COMPASS");
+            expect(result).to.deep.equal(expectedResult);
+        });
+
+        test("Get Notification From template undefined", () => {
+            const expectedResult = { body: "",  title: ""};
+            const result = NotificationService.getNotificationFromTemplate(testUser.firstName, "");
+            expect(result).to.deep.equal(expectedResult);
+        });
+
+        test("Send feedback notification", async () => {
+            const mockType = "FEEDBACK";
+            await notificationService.sendNotification(mockToken, testUser.firstName, mockType)
+                .then((result) => expect(result).to.not.be.undefined);
+        });
+
+        test("Send request feedback notification", async () => {
+            const mockType = "REQUEST";
+            await notificationService.sendNotification(mockToken, testUser.firstName, mockType)
+                .then((result) => expect(result).to.not.be.undefined);
+        });
+
+        test("Send compass  notification", async () => {
+            const mockType = "COMPASS";
+            await notificationService.sendNotification(mockToken, testUser.firstName, mockType)
+                .then((result) => expect(result).to.not.be.undefined);
+        });
+    });
+
+    suite("Sad cases", () => {
+
+        let notificationService : NotificationService;
+        let mockError : Error;
+        before(() => {
+            mockError = new Error("mock error");
+            const mockService :any = {
+                sendNotification: sinon.stub().rejects(mockError)
+            };
+            notificationService = new NotificationService(mockService);
+        });
+
+        test("Send compass  notification", async () => {
+            const mockType = "COMPASS";
+            await notificationService.sendNotification(mockToken, testUser.firstName, mockType)
+                .catch((err: Error) => expect(err).to.be.equal(mockError))
+        });
     });
 
 });
