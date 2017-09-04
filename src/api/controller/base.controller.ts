98553eb4ffa3ee212ca9aa8a9d0c7118ca8d3d7a
Peter Szabo <peter.szabo@hipteam.io>
2017-09-04 15:44:13 +0200

    Remove dead code + exclude schemas from test coverage

--- src/api/controller/base.controller.ts
+++ src/api/controller/base.controller.ts
@@ -4,16 +4,6 @@ import * as StatusCodes from 'http-status-codes';
 import { formError } from "../../util/errorhandler";
 
 class BaseController {
-    protected callController(promise: Promise<any>, res: Response, successCode: number, errorCode: number) {
-        promise
-            .then((result) => res.status(successCode).send(result))
-            .catch((error) => res.status(errorCode).json(ErrorHandler.formError(error)));
-    }
-
-    protected static send(res: any, code: number, response: any) {
-        res.status(code);
-        res.send(response);
-    }
 
     protected static getErrorStatus(err: any): number {
         return err.getStatusCode ? err.getStatusCode() : StatusCodes.INTERNAL_SERVER_ERROR
