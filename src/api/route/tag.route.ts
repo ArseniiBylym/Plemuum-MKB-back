25827915994166c2cbcde777d42c877b7b214b6a
Peter Szabo <peter.szabo@hipteam.io>
2017-09-27 11:49:29 +0200

    Check Tag API endpoints and docs

--- src/api/route/tag.route.ts
+++ src/api/route/tag.route.ts
@@ -2,28 +2,36 @@ import TagController from "../controller/tag.controller";
 import { Express } from "express";
 import * as passport from "passport";
 
-/**
- * @apiDefine tag_list_data
- * @apiSuccess (Success 200) {Tag[]} tags Id of the tag
- * @apiSuccess (Success 200) {String} _id Id of the tag
- * @apiSuccess (Success 200) {String} updatedAt Date of update in ISO Format
- * @apiSuccess (Success 200) {String} createdAt Date of creation in ISO Format
- * @apiSuccess (Success 200) {String} title The title of the tag
- * @apiSuccess (Success 200) {Boolean} isActive If the tag is active or not
- * @apiSuccess (Success 200) {Number} order The order of the tag
- */
 export default (app: Express, tagController: TagController) => {
 
     /**
-     * @api {POST} /api/:orgId/tag Create a new Tag
+     * @api {POST} /api/:orgId/tag Tag - Create a new Tag
      * @apiVersion 2.0.0
      * @apiName tag
-     * @apiGroup Tag
+     * @apiGroup Admin
      * @apiHeader Authorization basic
+     *
      * @apiParam {String} orgId Organization Id
      * @apiParam {String} title Title of the tag
      *
-     * @apiUse tag_list_data
+     * @apiSuccess (Success 201) {String} _id Id of the tag
+     * @apiSuccess (Success 201) {String} updatedAt Date of update in ISO Format
+     * @apiSuccess (Success 201) {String} createdAt Date of creation in ISO Format
+     * @apiSuccess (Success 201) {String} title The title of the tag
+     * @apiSuccess (Success 201) {Boolean} isActive If the tag is active or not
+     * @apiSuccess (Success 201) {Number} order The order of the tag
+     *
+     * @apiSuccessExample {json} Success-Response:
+     * HTTP/1.1 201 CREATED
+     * {
+     *     "_id": "59cb71c878ee0108d5e68ac1"
+     *     "title": "New tag",
+     *     "isActive": true,
+     *     "order": 7,
+     *     "createdAt": "2017-09-27T09:39:20.286Z",
+     *     "updatedAt": "2017-09-27T09:39:20.286Z",
+     * }
+     *
      */
     app.route("/api/:orgId/tag")
         .post(passport.authenticate('basic', {session: false}), tagController.addNewTag.bind(tagController));
@@ -37,31 +45,32 @@ export default (app: Express, tagController: TagController) => {
      * @apiHeader {String} Authorization Bearer token
      * @apiParam {String} orgId Organization id
      *
-     * @apiUse tag_list_data
+     * @apiSuccess (Success 200) {Tag[]} - Array of tags
      *
      * @apiSuccessExample {json} Success-Response:
-     *     HTTP/1.1 200 OK
-     *       [
-     *           {
-     *               "_id": "5989b97ddc5d02286b4dd531",
-     *               "updatedAt": "2017-08-08T13:15:41.950Z",
-     *               "createdAt": "2017-08-08T13:15:41.950Z",
-     *               "title": "TestTitle",
-     *               "isActive": true,
-     *               "order": 1
-     *           },
-     *           {
-     *               "_id": "5989ba5bdc5d02286b4dd532",
-     *               "updatedAt": "2017-08-08T13:19:23.001Z",
-     *               "createdAt": "2017-08-08T13:19:23.001Z",
-     *               "title": "TestTitle2",
-     *               "isActive": true,
-     *               "order": 2
-     *           }
-     *       ]
+     * HTTP/1.1 200 OK
+     *   [
+     *       {
+     *           "_id": "5989b97ddc5d02286b4dd531",
+     *           "updatedAt": "2017-08-08T13:15:41.950Z",
+     *           "createdAt": "2017-08-08T13:15:41.950Z",
+     *           "title": "TestTitle",
+     *           "isActive": true,
+     *           "order": 1
+     *       },
+     *       {
+     *           "_id": "5989ba5bdc5d02286b4dd532",
+     *           "updatedAt": "2017-08-08T13:19:23.001Z",
+     *           "createdAt": "2017-08-08T13:19:23.001Z",
+     *           "title": "TestTitle2",
+     *           "isActive": true,
+     *           "order": 2
+     *       },
+     *
+     *       ...
+     *   ]
      */
 
     app.route("/api/:orgId/tags")
         .get(passport.authenticate('bearer', {session: false}), tagController.getTags.bind(tagController));
-
 }
