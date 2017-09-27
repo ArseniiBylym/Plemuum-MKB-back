75ea6585b0a410af291d01864977c5933d61633f
Peter Szabo <peter.szabo@hipteam.io>
2017-09-27 17:12:56 +0200

    Add staging config

--- config/config.ts
+++ config/config.ts
@@ -1,6 +1,7 @@
 import devConfig from "./dev.config"
 import prodConfig from "./prod.config"
 import testConfig from "./test.config"
+import stagingConfig from "./staging.config"
 import { Config } from "./config.interface";
 
 const getConfig = (): Config => {
@@ -11,6 +12,8 @@ const getConfig = (): Config => {
             return prodConfig;
         case "test":
             return testConfig;
+        case "staging":
+            return stagingConfig;
         default:
             console.error('NODE_ENV environment variable not set | dev will be used');
             return devConfig;
