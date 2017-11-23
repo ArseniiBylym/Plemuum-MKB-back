fce518e2617d48b8991b2d3dd89b2618db354718
Peter Szabo <peter.szabo@hipteam.io>
2017-11-23 14:12:35 +0100

    Fix failing file transfer service test

--- src/service/file/filetransfer.service.ts
+++ src/service/file/filetransfer.service.ts
@@ -3,17 +3,17 @@ import StorageManager from './storage.manager';
 import { ErrorType, PlenuumError } from "../../util/errorhandler";
 
 export default class FileTransferService {
-
-    private storageManager: StorageManager;
-    private SUPPORTED_IMAGE_EXTENSIONS = ['jpg', 'jpeg', 'png', 'gif'];
-    private MAX_FILE_SIZE = 1048576; // 1 MB;
+    public static SUPPORTED_IMAGE_EXTENSIONS = ['jpg', 'jpeg', 'png', 'gif'];
+    public static MAX_FILE_SIZE = 1048576; // 1 MB;
     // https://asecuritysite.com/forensics/magic
-    private MAGIC_NUMBERS: any = {
+    public static MAGIC_NUMBERS: any = {
         'jpg': Buffer.from([parseInt('FF', 16), parseInt('D8', 16)]),
         'jpeg': Buffer.from([parseInt('FF', 16), parseInt('D8', 16)]),
         'gif': Buffer.from([parseInt('47', 16), parseInt('49', 16), parseInt('46', 16), parseInt('38', 16)]),
         'png': Buffer.from([parseInt('89', 16), parseInt('50', 16), parseInt('4E', 16), parseInt('47', 16)])
     };
+
+    private storageManager: StorageManager;
 
     constructor(storageManager: StorageManager) {
         this.storageManager = storageManager;
@@ -46,23 +46,22 @@ export default class FileTransferService {
     }
 
     private validFileSize(file: any) {
-        return file.size <= this.MAX_FILE_SIZE;
+        return file.size <= FileTransferService.MAX_FILE_SIZE;
     }
 
     private async validExtension(file: any) {
         const fileType = file.type.split("/");
-        return (fileType[0] === 'image' && this.SUPPORTED_IMAGE_EXTENSIONS.indexOf(fileType[1]) !== -1)
+        return (fileType[0] === 'image' && FileTransferService.SUPPORTED_IMAGE_EXTENSIONS.indexOf(fileType[1]) !== -1)
             && await this.validContent(file, fileType[1])
     }
 
     private async validContent(file: any, extension: string) {
-        if (!this.MAGIC_NUMBERS[extension]) return false; // Unsupported extension
+        if (!FileTransferService.MAGIC_NUMBERS[extension]) return false; // Unsupported extension
 
-        const buffer = new Buffer(this.MAGIC_NUMBERS[extension].byteLength);
+        const buffer = new Buffer(FileTransferService.MAGIC_NUMBERS[extension].byteLength);
 
         const fd = await fs.open(file.path, 'r');
-        await fs.read(fd, buffer, 0, this.MAGIC_NUMBERS[extension].byteLength, 0);
-
-        return buffer.compare(this.MAGIC_NUMBERS[extension]) === 0;
+        await fs.read(fd, buffer, 0, FileTransferService.MAGIC_NUMBERS[extension].byteLength, 0);
+        return buffer.compare(FileTransferService.MAGIC_NUMBERS[extension]) === 0;
     }
 }
--- src/test/service/filetransfer.service.spec.ts
+++ src/test/service/filetransfer.service.spec.ts
@@ -8,19 +8,26 @@ suite("Filetransfer tests", () => {
     test("uploadUserPicture", async () => {
         const url = "plennum.com/plenuum/userPictures/userId";
         const picture = {
-            path: "path"
+            path: "path",
+            type: "image/jpeg",
+            size: 1000000
         };
 
         const storageManager = {
             uploadFile: sinon.stub()
-        }
+        };
 
         storageManager.uploadFile.withArgs("plenuum/userPictures", "userId".valueOf(), picture.path).resolves(url);
         const unlink = sinon.stub(fs, "unlink");
+        const open = sinon.stub(fs, "open");
+        const incomingBuffer = new Buffer(FileTransferService.MAGIC_NUMBERS['jpeg']);
+        const read = sinon.stub(fs, "read").callsFake((fd, buffer) => incomingBuffer.copy(buffer));
 
         const fileTransferService = new FileTransferService(storageManager);
-        const result = await fileTransferService.uploadUserPicture(picture, "userId")
+        const result = await fileTransferService.uploadUserPicture(picture, "userId");
         unlink.restore();
+        open.restore();
+        read.restore();
 
         expect(result).to.be.equal(url);
         sinon.assert.called(unlink);
