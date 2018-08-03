1644dd00b13547579d78eb6748a5890d15bd1f6d
Szimkovics Tamás <tamas.szimkovics@pettersonapps.com>
2018-08-03 18:26:34 +0300

    Suspend "Survey result" email. PLENUUM-124

--- src/api/interactor/survey.interactor.ts
+++ src/api/interactor/survey.interactor.ts
@@ -136,8 +136,10 @@ export default class SurveyInteractor {
         const result = await SurveyDataController.saveSurveyTodo(orgId, surveyTodo);
         const {_id, respondent} = result;
         if (_id){
-            const surveyWithAnswers = await SurveyDataController.getSurveyTodo(orgId, _id, respondent);
-            return await agenda.schedule(new Date(Date.now() + 2000),'sendSurveyAnswers',  surveyWithAnswers);
+            return await SurveyDataController.getSurveyTodo(orgId, _id, respondent);
+
+            // const surveyWithAnswers = await SurveyDataController.getSurveyTodo(orgId, _id, respondent);
+            // return await agenda.schedule(new Date(Date.now() + 2000),'sendSurveyAnswers',  surveyWithAnswers);
         }
     }
 
